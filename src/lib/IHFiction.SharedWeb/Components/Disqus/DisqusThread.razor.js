86a797100c16db48169d862f0f9bfa7014363149
Bryan Gonzalez <bgonza868@gmail.com>
2026-06-19 14:09:38 -0400

    Add nonce support to Disqus integration for enhanced security

--- src/lib/IHFiction.SharedWeb/Components/Disqus/DisqusThread.razor.js
+++ src/lib/IHFiction.SharedWeb/Components/Disqus/DisqusThread.razor.js
@@ -1,4 +1,4 @@
-export function loadDisqus(pageUrl, pageIdentifier, shortName) {
+export function loadDisqus(pageUrl, pageIdentifier, shortName, nonce) {
     const disqus_config = () => {
         this.page.url = pageUrl;
         this.page.identifier = pageIdentifier;
@@ -12,6 +12,9 @@ export function loadDisqus(pageUrl, pageIdentifier, shortName) {
     } else {
         const d = document, s = d.createElement('script');
         s.src = `https://${shortName}.disqus.com/embed.js`;
+        if (nonce) {
+            s.nonce = nonce;
+        }
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
     }
